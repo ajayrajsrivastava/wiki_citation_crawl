13e2ecb7230221dc0104677e28a3e6eaab9e8655
Ajay Raj Srivastava <ajayisready@gmail.com>
2016-11-04 03:20:43 +0530

    Update script.py

--- script.py
+++ script.py
@@ -37,10 +37,10 @@ class WikiCrawl:
                         #Query-2
     
     def printCitations(self,startIndex,para):
-
+    
         endIndex = para[startIndex:].find(']')
         print para[startIndex+1:startIndex+endIndex] #[Any num of digits] 
-        nextCit = endIndex+1
+        nextCit = startIndex+endIndex+1 
         if nextCit<= len(para)-1 and para[nextCit] == '[':   #In case of Multiple Citations eg. [1][2][3]...
             self.printCitations(nextCit,para)
             
@@ -55,13 +55,13 @@ class WikiCrawl:
                 startIndex = para.find(lines)+len(lines)+1 #'['
                 self.printCitations(startIndex,para)
     
-        if InPara != True:
-            #Search Table
-            for td_tags in self.soup.find_all('td'):
-                tableText = str(td_tags.text.encode('utf-8'))
-                if lines in tableText:
-                    startIndex = tableText.find(lines)+len(lines)+1 #'['
-                    printCitations(startIndex,tableText)
+            if InPara != True:
+                #Search Table
+                for td_tags in self.soup.find_all('td'):
+                    tableText = str(td_tags.text.encode('utf-8'))
+                    if lines in tableText:
+                        startIndex = tableText.find(lines)+len(lines) #'['
+                        self.printCitations(startIndex,tableText)
 
     
     def getQuery(self):
@@ -79,7 +79,7 @@ class WikiCrawl:
         try:
             self.link = urllib.urlopen(URL) 
         except Exception:
-            traceback.print_exc() #error stack trace
+            traceback.print_exc() 
         
         self.soup = BeautifulSoup(self.link,'html.parser') #html parsing
         self.getQuery() 
